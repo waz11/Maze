a9b9e71d1e24a902261131493339f5d0a6b8b48d
waz11 <ronisr@post.bgu.ac.il>
2020-04-03 13:17:47 +0300

    ron

--- src/algorithms/search/BreadthFirstSearch.java
+++ src/algorithms/search/BreadthFirstSearch.java
@@ -3,41 +3,36 @@ package algorithms.search;
 import java.util.*;
 
 public class BreadthFirstSearch extends ASearchingAlgorithm {
-    /*
-    private Solution solution;
-    private Stack<AState> states;
-    private Hashtable<String, AState> grayStates;
-    */
-
 
     public Solution solve(ISearchable s) {
-        Solution solution = new Solution();
-        Hashtable<String, AState> gratStates = new Hashtable<>();
+        Solution finalSolution = new Solution();
+        Hashtable<String, AState> visited = new Hashtable<>();
         Queue<AState> toVisit = new LinkedList<>();
 
-        if (s == null)
-            return solution;
+        if (s==null)
+            return finalSolution;
 
         AState start = s.getStartState();
         AState goal = s.getGoalState();
         toVisit.add(start);
 
-        while (toVisit.size() != 0) {
+        while (toVisit.size() != 0){
             AState curr = toVisit.poll();
-            if (curr.equals(goal)) {
-                solution = createSolution(curr, start);
-            } else {
+            if (curr.equals(goal)){
+                finalSolution = createSolution(curr, start);
+            }
+            else{
                 this.length++;
 
                 LinkedList<AState> possible = s.getAllSuccessors(curr);
-                for (int i = 0; i < possible.size(); i++) {
-                    if (!(gratStates.containsKey(possible.get(i).toString()))) {
-                        gratStates.put(possible.get(i).toString(), possible.get(i));
+                for (int i=0; i<possible.size(); i++){
+                    if(!(visited.containsKey(possible.get(i).toString()))){
+                        visited.put(possible.get(i).toString(), possible.get(i));
                         toVisit.add(possible.get(i));
                     }
                 }
             }
         }
-        return solution;
+        return finalSolution;
     }
 }
